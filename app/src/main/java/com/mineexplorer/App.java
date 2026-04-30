44f38b403a37ee04602269644d8bf9ff41304d45
Bastien Chanez <bastien.chanez@gmail.com>
2026-04-30 15:28:10 +0200

    feat: integrate Spring Boot and update project dependencies

--- app/src/main/java/com/mineexplorer/App.java
+++
@@ -1,5 +0,0 @@
-package com.mineexplorer;
-
-public class App {
-    // Spring Boot application will be configured here
-}
---
+++ app/src/main/java/com/mineexplorer/MineExplorerApplication.java
@@ -0,0 +1,12 @@
+package com.mineexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MineExplorerApplication.class, args);
+    }
+}
